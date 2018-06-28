472ef01cf91a4eee2b6459c93e842ed1ba83aa96
Colin Lemus <Colin@Colins-MacBook-Pro.local>
2018-06-27 21:12:25 -0700

    lit

---
+++ assets/script.js
@@ -0,0 +1,34 @@
+var $search = $("#search");
+var $clearResults = $("#clearResults");
+var searchTerm;
+var numberOfRecords;
+var startYear;
+var endYear;
+
+function search() {
+    $search.on("click", function() {
+
+    }) ;
+}
+
+function clearResults() {
+    $clearResults.on("click", function() {
+
+    });
+}
+
+function searchTerm() {
+
+}
+
+function numberOfRecords() {
+
+}
+
+function startYear() {
+
+}
+
+function endYear() {
+    
+}
